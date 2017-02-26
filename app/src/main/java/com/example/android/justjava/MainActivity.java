c7a48c1aab0670ebe9f7342fd58b608256bbcc95
ennio nasca <ennionasca@MacBook-Pro-di-ennio.local>
2017-02-26 22:20:24 +0100

    addead checkbox for toppings

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,6 +10,7 @@ import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import static android.R.attr.max;
@@ -21,6 +22,7 @@ import static android.R.id.message;
 public class MainActivity extends AppCompatActivity {
 
     private int quantity = 0;
+    private int cupOfCoffe = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +34,35 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String message = "Total: " + quantity*5.0 + "€\nThank you!";
+        float price = calculatePrice();
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.whipped_cream_box);
+        boolean hasWhippedCream = checkBox1.isChecked();
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.chocolate_box);
+        boolean hasChocolate = checkBox2.isChecked();
+        String message = createOrderSummary(price,hasWhippedCream,hasChocolate);
         displayMessage(message);
+    }
+
+    /**
+     * Creates the summary of the order for the currunt purchase
+     */
+    public String createOrderSummary(float price, boolean addWhippedCream, boolean addChocolate){
+        String summary = "Name: Ennio Nasca"
+                +"\nadd whipped cream? " + addWhippedCream
+                +"\nadd chocolate? " + addChocolate
+                +"\nQuantity: " + quantity
+                +"\nTotal: €" + price
+                +"\nThank You!";
+        return summary;
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     */
+    private float calculatePrice() {
+        float price = quantity * cupOfCoffe;
+        return price;
     }
 
     /**
@@ -45,12 +74,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayPrice(int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     private void displayMessage(String message){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(message);
     }
 
